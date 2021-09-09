dfb9440b6aeaa9db95eff9873537ed7ce10a1c94
aaravgupta99 <aaravg1999@gmail.com>
2021-09-09 22:15:10 +0530

    main accepted incoming change

--- JS.js
+++ JS.js
@@ -4,8 +4,4 @@ console.log(string)
 a = string.substr(0, 5)
 console.log(a + " Aarav")
 console.log("abcd")
-<<<<<<< HEAD
-console.log("whatsup?")
-=======
 console.log("ab")
->>>>>>> bc6d6b497c4c056bf41d4ac87a8ac3cf3d3452f6
