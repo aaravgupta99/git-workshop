6cc55c715c71414de7c7174f451e66393984b6f0
aaravgupta99 <aaravg1999@gmail.com>
2021-09-09 22:13:53 +0530

    again accepted change from new-feature to development

--- JS.js
+++ JS.js
@@ -8,12 +8,8 @@ console.log("abcd")
 console.log("whatsup?")
 console.log("ab")
 
-<<<<<<< HEAD
-console.log("In the development branch")
-=======
 console.log("new-feature branch")
 calculate(x, y)
 {
   console.log(x+y)
 }
->>>>>>> 1ec2dd7bea53a6ddc17c3c87936955d4c933e283
